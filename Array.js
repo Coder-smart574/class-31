276c1128df80c472bb6dab5a2c6fa37e3becab22
Coder-smart574 <73115351+Coder-smart574@users.noreply.github.com>
2020-12-23 18:01:28 +0530

    Hi Bye

---
+++ Array.js
@@ -0,0 +1,23 @@
+//Examples of different types of data stored in javascript
+
+//string
+var string = "Priyansh";
+console.log(string);
+
+//number
+var num = 100;
+console.log(num);
+
+//boolean
+var bool = true;
+console.log(bool);
+
+//object undefined
+var object;
+console.log(object);
+
+//null
+object = null;
+console.log(object);
+
+
